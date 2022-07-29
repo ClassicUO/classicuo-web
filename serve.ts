b15de5396d80addf48e5bb5e5e2f805a5d2a5ea6
jabin Bastian <this@jabin.id.au>
2022-07-29 17:26:48 +1000

    Update links to https://play.classicuo.org

--- serve.ts
+++ serve.ts
@@ -1,10 +1,20 @@
-import Server from "lume/core/server.ts";
+import Server, { Middleware } from "lume/core/server.ts";
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_site`,
 });
 
+const redirectMiddleware: Middleware = (req, next, _conn) => {
+  const { pathname, origin } = new URL(req.url);
+  if(/^\/docs\/?$/i.test(pathname)) {
+    return Promise.resolve(Response.redirect(`${origin}/docs/intro`));
+  }
+
+  return next(req);
+}
+
+server.use(redirectMiddleware);
 server.start();
 
 console.log("Listening on http://localhost:8000");
