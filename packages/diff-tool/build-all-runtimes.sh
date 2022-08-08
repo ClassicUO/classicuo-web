e33e3de241582c7f7788253ec3962dbeb03ef1d7
jabin Bastian <this@jabin.id.au>
2022-08-08 11:05:00 +1000

    Remove WASI

--- packages/diff-tool/build-all-runtimes.sh
+++ packages/diff-tool/build-all-runtimes.sh
@@ -5,5 +5,5 @@ declare -a runtimes=("win-x64" "linux-x64" "osx.12-x64" "osx.12-arm64")
 for i in "${runtimes[@]}"
 do
    echo "Bulding for runtime: $i"
-   dotnet publish src/ClassicUO-Web-FileDiff.csproj -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=true -p:SelfContained=true -c Release -r $i
+   dotnet publish src/DiffTool.csproj -p:PublishSingleFile=true -p:PublishReadyToRun=true -p:PublishTrimmed=true -p:SelfContained=true -c Release -r $i
 done
